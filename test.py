f907fba9e7c92493ac035c4ae639b57033ca73c4
Nwankwo Onyedika Benson <nwankwobenson29@gmail.com>
2024-08-13 16:56:34 +0100

    Made all other routes work

--- test.py
+++ test.py
@@ -1,14 +1,17 @@
 from flask import Flask
 from flask import render_template
-from flask import redirect
-from flask import url_for
-
 app = Flask(__name__)
-
-
 @app.route("/")
 def home():
     return render_template('index.html')
-
+@app.route("/projects")
+def projects():
+    return render_template("projects.html")
+app.route("/blog")
+def blog():
+    return render_template("blog.html")
+app.route("/about")
+def about():
+    return render_template("about.html")
 if __name__ == "__main__":
-    app.run(
+    app.run()
